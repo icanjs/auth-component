d7449933abe70ec9bf93579e584fb82de5fd4c61
Marshall Thompson <marshall@creativeideal.net>
2016-05-12 07:17:40 -0700

    Pretty icons.

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,6 +3,7 @@ import Map from 'can/map/';
 import 'can/map/define/';
 import './auth.less!';
 import template from './auth.stache';
+import providerIcons from '../provider-icons/index';
 
 export const ViewModel = Map.extend({
   define: {
@@ -19,10 +20,92 @@ export const ViewModel = Map.extend({
         }
       },
 
+      /**
+       * Tabs should only show if local login is set to password and signup is enabled.
+       */
       tabsShouldShow: {
         get(){
           console.log('hi');
           return this.attr('local') === 'password' && this.attr('localSignup');
+        }
+      },
+
+      /**
+       * A comma-separated string of SSO providers to enable. The icons will show
+       * in the UI based on the order they they are provided here.
+       */
+      providers: {
+        type: 'string'
+      },
+
+      /**
+       * An array of the passed-in providers. Used to put the icons in the correct
+       * order.
+       */
+      providerList: {
+        get(){
+          let providerString = this.attr('providers'),
+            providerList;
+
+          if (providerString) {
+            providerList = providerString.split(',')
+              // filter out any duds.
+              .filter(provider => {
+                let providerArray = Object.keys(providerIcons);
+                return providerArray.indexOf(provider.trim()) > -1;
+              })
+              .map(provider => {
+                if (provider) {
+                  provider = provider.trim();
+                  return {
+                    name: provider,
+                    template: providerIcons[provider]
+                  };
+                }
+              });
+          }
+          return providerList;
+        }
+      },
+
+      providerIcons: {
+        value: providerIcons
+      },
+
+      /**
+       * Returns an integer representing the number of SSO providers that have
+       * been enabled.
+       */
+      providerCount: {
+        get(){
+          let providers = this.attr('providerList');
+          return providers.length || 0;
+        }
+      },
+
+      /**
+       * This returns a word to be used in a class to help with the button sizing
+       * for configured SSO provider(s).  If there is only one SSO provider
+       * configured, the button will be larger and centered.  For two providers,
+       * there will be two centered buttons.  For more than 2 providers, there
+       * will be an icon representing each one (with no text to the side of
+       * the icon.)
+       */
+      providerCountClass: {
+        get(){
+          let providers = this.attr('providerList');
+          if (providers){
+            if(providers.length === 1) {
+              return 'one';
+            }
+            if (providers && providers.length === 2) {
+              return 'two';
+            }
+            if (providers.length > 2) {
+              return 'many';
+            }
+          }
+          return providers && providers.length;
         }
       },
 
