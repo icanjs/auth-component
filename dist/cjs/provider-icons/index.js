cde635fad176507a86ba0b2f5de363319de2a09b
Marshall Thompson <marshall@creativeideal.net>
2016-07-17 20:43:00 -0600

    3.0.3

---
+++ dist/cjs/provider-icons/index.js
@@ -0,0 +1,43 @@
+/*auth-component@3.0.2#provider-icons/index*/
+'use strict';
+var $__amazon_46_stache__, $__dropbox_46_stache__, $__evernote_46_stache__, $__facebook_46_stache__, $__github_46_stache__, $__google_46_stache__, $__linkedin_46_stache__, $__microsoft_46_stache__, $__openid_46_stache__, $__paypal_46_stache__, $__skype_46_stache__, $__slack_46_stache__, $__stackoverflow_46_stache__, $__twitter_46_stache__, $__yahoo_46_stache__;
+var amazon = ($__amazon_46_stache__ = require('./amazon.stache.js'), $__amazon_46_stache__ && $__amazon_46_stache__.__esModule && $__amazon_46_stache__ || { default: $__amazon_46_stache__ }).default;
+var dropbox = ($__dropbox_46_stache__ = require('./dropbox.stache.js'), $__dropbox_46_stache__ && $__dropbox_46_stache__.__esModule && $__dropbox_46_stache__ || { default: $__dropbox_46_stache__ }).default;
+var evernote = ($__evernote_46_stache__ = require('./evernote.stache.js'), $__evernote_46_stache__ && $__evernote_46_stache__.__esModule && $__evernote_46_stache__ || { default: $__evernote_46_stache__ }).default;
+var facebook = ($__facebook_46_stache__ = require('./facebook.stache.js'), $__facebook_46_stache__ && $__facebook_46_stache__.__esModule && $__facebook_46_stache__ || { default: $__facebook_46_stache__ }).default;
+var github = ($__github_46_stache__ = require('./github.stache.js'), $__github_46_stache__ && $__github_46_stache__.__esModule && $__github_46_stache__ || { default: $__github_46_stache__ }).default;
+var google = ($__google_46_stache__ = require('./google.stache.js'), $__google_46_stache__ && $__google_46_stache__.__esModule && $__google_46_stache__ || { default: $__google_46_stache__ }).default;
+var linkedin = ($__linkedin_46_stache__ = require('./linkedin.stache.js'), $__linkedin_46_stache__ && $__linkedin_46_stache__.__esModule && $__linkedin_46_stache__ || { default: $__linkedin_46_stache__ }).default;
+var microsoft = ($__microsoft_46_stache__ = require('./microsoft.stache.js'), $__microsoft_46_stache__ && $__microsoft_46_stache__.__esModule && $__microsoft_46_stache__ || { default: $__microsoft_46_stache__ }).default;
+var openid = ($__openid_46_stache__ = require('./openid.stache.js'), $__openid_46_stache__ && $__openid_46_stache__.__esModule && $__openid_46_stache__ || { default: $__openid_46_stache__ }).default;
+var paypal = ($__paypal_46_stache__ = require('./paypal.stache.js'), $__paypal_46_stache__ && $__paypal_46_stache__.__esModule && $__paypal_46_stache__ || { default: $__paypal_46_stache__ }).default;
+var skype = ($__skype_46_stache__ = require('./skype.stache.js'), $__skype_46_stache__ && $__skype_46_stache__.__esModule && $__skype_46_stache__ || { default: $__skype_46_stache__ }).default;
+var slack = ($__slack_46_stache__ = require('./slack.stache.js'), $__slack_46_stache__ && $__slack_46_stache__.__esModule && $__slack_46_stache__ || { default: $__slack_46_stache__ }).default;
+var stackoverflow = ($__stackoverflow_46_stache__ = require('./stackoverflow.stache.js'), $__stackoverflow_46_stache__ && $__stackoverflow_46_stache__.__esModule && $__stackoverflow_46_stache__ || { default: $__stackoverflow_46_stache__ }).default;
+var twitter = ($__twitter_46_stache__ = require('./twitter.stache.js'), $__twitter_46_stache__ && $__twitter_46_stache__.__esModule && $__twitter_46_stache__ || { default: $__twitter_46_stache__ }).default;
+var yahoo = ($__yahoo_46_stache__ = require('./yahoo.stache.js'), $__yahoo_46_stache__ && $__yahoo_46_stache__.__esModule && $__yahoo_46_stache__ || { default: $__yahoo_46_stache__ }).default;
+var $__default = {
+    amazon: amazon,
+    dropbox: dropbox,
+    evernote: evernote,
+    facebook: facebook,
+    github: github,
+    google: google,
+    linkedin: linkedin,
+    microsoft: microsoft,
+    openid: openid,
+    paypal: paypal,
+    skype: skype,
+    slack: slack,
+    stackoverflow: stackoverflow,
+    twitter: twitter,
+    yahoo: yahoo
+};
+Object.defineProperties(module.exports, {
+    default: {
+        get: function () {
+            return $__default;
+        }
+    },
+    __esModule: { value: true }
+});
