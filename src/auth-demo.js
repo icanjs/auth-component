c198a4e233197276ee73be03dd61fd8b006d824e
Marshall Thompson <marshall@creativeideal.net>
2016-12-22 00:44:31 -0700

    Started reorganizing demo.

--- src/auth-demo.js
+++ src/auth-demo.js
@@ -1,10 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AuthDemo from './auth-demo.jsx';
+import AmazonButton from './buttons/amazon/';
+import DropboxButton from './buttons/dropbox/';
+import EvernoteButton from './buttons/evernote/';
+import FacebookButton from './buttons/facebook/';
+import GitHubButton from './buttons/github/';
+import GoogleButton from './buttons/google/';
+import LinkedInButton from './buttons/linkedin/';
+import MicrosoftButton from './buttons/microsoft/';
+import OpenIDButton from './buttons/openid/';
+import PaypalButton from './buttons/paypal/';
+import SkypeButton from './buttons/skype/';
+import SlackButton from './buttons/slack/';
+import TwitterButton from './buttons/twitter/';
+import YahooButton from './buttons/yahoo/';
+
+var signupActive = subpage === 'signup' ? 'active' : '';
+var loginActive = subpage === 'login' ? 'active' : '';
 
 ReactDOM.render(
   <div>
-    <AuthDemo />
+    // TODO: import the auth.js component and use it. Add routing.
+    <div className='auth-component'>
+      <div className='auth-ui-container'>
+        <ul className='local-auth-tabs'>
+          <li className={`signup ${signupActive}`} onClick={setSubpage('signup')}>
+            <a href='javascript://'>Signup</a>
+          </li>
+          <li className={`login ${loginActive}`} onClick={setSubpage('login')}>
+            <a href='javascript://'>Login</a>
+          </li>
+        </ul>
+
+        <div className='auth-branding'>
+          <content />
+        </div>
+
+        <div className='sso-providers one'>
+          <AmazonButton popup='true' />
+          <DropboxButton popup='true' />
+          <EvernoteButton popup='true' />
+          <FacebookButton popup='true' />
+          <GitHubButton popup='true' />
+          <GoogleButton popup='true' />
+          <LinkedInButton popup='true' />
+          <MicrosoftButton popup='true' />
+          <OpenIDButton popup='true' />
+          <PaypalButton popup='true' />
+          <SkypeButton popup='true' />
+          <SlackButton popup='true' />
+          <TwitterButton popup='true' />
+          <YahooButton popup='true' />
+        </div>
+      </div>
+    </div>
   </div>,
   document.querySelector('[root=true]')
 );
--- src/auth-demo.jsx
+++ src/auth-demo.jsx
@@ -1,57 +1,10 @@
 import React from 'react';
-import AmazonButton from './buttons/amazon/';
-import DropboxButton from './buttons/dropbox/';
-import EvernoteButton from './buttons/evernote/';
-import FacebookButton from './buttons/facebook/';
-import GitHubButton from './buttons/github/';
-import GoogleButton from './buttons/google/';
-import LinkedInButton from './buttons/linkedin/';
-import MicrosoftButton from './buttons/microsoft/';
-import OpenIDButton from './buttons/openid/';
-import PaypalButton from './buttons/paypal/';
-import SkypeButton from './buttons/skype/';
-import SlackButton from './buttons/slack/';
-import TwitterButton from './buttons/twitter/';
-import YahooButton from './buttons/yahoo/';
+
 import './auth.less';
 
 export default ({subpage, setSubpage}) => {
-  var signupActive = subpage === 'signup' ? 'active' : '';
-  var loginActive = subpage === 'login' ? 'active' : '';
-
+  
   return (
-    <div className='auth-component'>
-      <div className='auth-ui-container'>
-        <ul className='local-auth-tabs'>
-          <li className={`signup ${signupActive}`} onClick={setSubpage('signup')}>
-            <a href='javascript://'>Signup</a>
-          </li>
-          <li className={`login ${loginActive}`} onClick={setSubpage('login')}>
-            <a href='javascript://'>Login</a>
-          </li>
-        </ul>
-
-        <div className='auth-branding'>
-          <content />
-        </div>
-
-        <div className='sso-providers one'>
-          <AmazonButton popup='true' />
-          <DropboxButton popup='true' />
-          <EvernoteButton popup='true' />
-          <FacebookButton popup='true' />
-          <GitHubButton popup='true' />
-          <GoogleButton popup='true' />
-          <LinkedInButton popup='true' />
-          <MicrosoftButton popup='true' />
-          <OpenIDButton popup='true' />
-          <PaypalButton popup='true' />
-          <SkypeButton popup='true' />
-          <SlackButton popup='true' />
-          <TwitterButton popup='true' />
-          <YahooButton popup='true' />
-        </div>
-      </div>
-    </div>
+    
   );
 };
