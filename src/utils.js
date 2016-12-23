9995fe98d36a0f99fcef6a8e64b1a14e4613aa45
Marshall Thompson <marshall@creativeideal.net>
2016-12-22 22:47:59 -0700

    Added dev util for logging.

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+/**
+ * `devWarning` is a logging utility that does a console.warn in localhost (dev environment).
+ */
+export function devWarning (message) {
+  if (window.location.hostname === 'localhost') {
+    console.warn(message + `This message will not show in production.`);
+  }
+}
