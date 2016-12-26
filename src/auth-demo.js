632ba997e7211b5ba9a51d7661f93b2c768339fa
Marshall Thompson <marshall@creativeideal.net>
2016-12-26 00:37:29 -0700

    Clean up the demo.

--- src/auth-demo.js
+++ src/auth-demo.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import route from 'can-route';
 import DefineMap from 'can-define/map/map';
-import AuthComponent from './auth';
+import {Route} from 'can-route-react';
+
+import SVGInline from 'react-svg-inline';
+import logo from './demo-logo.svg';
+
+import AuthContainer from './auth-container/auth-container';
+import Tabs from './tabs/can-route';
+import FacebookButton from './buttons/facebook/';
+import GitHubButton from './buttons/github/';
+import GoogleButton from './buttons/google/';
+import MicrosoftButton from './buttons/microsoft/';
+import TwitterButton from './buttons/twitter/';
+import SignupForm from 'auth-component/forms/signup/';
+import LoginForm from 'auth-component/forms/login/';
 
 const RouteMap = DefineMap.extend({
   page: {
@@ -17,6 +30,23 @@ route('{page}', {page: 'login'});
 route.ready();
 
 ReactDOM.render(
-  <AuthComponent activeTab={route.data.page} />,
+  <AuthContainer>
+    <Tabs activeTab={route.data.page} routeAttr='page' />
+
+    <div className='auth-branding'>
+      <SVGInline svg={logo} />
+    </div>
+
+    <div className='oauth-buttons'>
+      <FacebookButton popup='true' />
+      <GitHubButton popup='true' />
+      <GoogleButton popup='true' />
+      <MicrosoftButton popup='true' />
+      <TwitterButton popup='true' />
+    </div>
+
+    <Route data={{page: 'login'}} component={LoginForm} />
+    <Route data={{page: 'signup'}} component={SignupForm} />
+  </AuthContainer>,
   document.querySelector('[root=true]')
 );
