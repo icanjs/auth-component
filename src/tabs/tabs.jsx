7f505411fc5e027b69ab7c7385c5b1aff3707cab
Marshall Thompson <marshall@creativeideal.net>
2016-12-26 00:22:28 -0700

    Tabs: Give each button its own click handler.

--- src/tabs/tabs.jsx
+++ src/tabs/tabs.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default ({activeTab, setTab}) => {
+export default ({activeTab, signupClicked, loginClicked}) => {
   let signupActive = activeTab === 'signup' ? 'active' : '';
   let loginActive = activeTab === 'login' ? 'active' : '';
 
   return (
     <ul className='auth-component-tabs'>
-      <li className={`signup ${signupActive}`} onClick={setTab('signup')}>
+      <li className={`signup ${signupActive}`} onClick={signupClicked}>
         <a href='javascript://'>Signup</a>
       </li>
-      <li className={`login ${loginActive}`} onClick={setTab('login')}>
+      <li className={`login ${loginActive}`} onClick={loginClicked}>
         <a href='javascript://'>Login</a>
       </li>
     </ul>
