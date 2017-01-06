ff49cc71348cf35dd63519aaf4bf55a777834f17
Marshall Thompson <marshall@creativeideal.net>
2017-01-05 21:16:46 -0700

    Fix steal config.
    system config —> steal config

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ var stealTools = require('steal-tools');
 var path = require('path');
 
 stealTools.export({
-  system: {
+  steal: {
     config: path.join(__dirname, '/package.json!npm')
   },
   outputs: {
