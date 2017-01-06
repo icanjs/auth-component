b915332a9e9a56eff29c9092f7493c202f9d19e4
Marshall Thompson <marshall@creativeideal.net>
2017-01-06 00:01:51 -0700

    Add per-field error handling.

--- src/forms/form-base-vm.js
+++ src/forms/form-base-vm.js
@@ -34,13 +34,36 @@ export default DefineMap.extend({
    */
   usernameChanged (event) {
     this.username = event.target.value;
-    this.onUsernameChange(event.target.value);
+    this.onUsernameChange(event.target.value)
+      .then(username => this.uiUsernameSuccess(username))
+      .catch(error => this.uiUsernameError(error));
   },
   /**
    * The `onUsernameChange` callback gets called right after the username
    * is updated. It can be used to check if a username is available.
    */
-  onUsernameChange (username) {},
+  onUsernameChange (username) {
+    return Promise.resolve();
+  },
+  /**
+   * If there is a problem with the username, `usernameError` can be set with an
+   * object that contains a 'message' attribute.
+   */
+  usernameError: 'any',
+  /**
+   * Clears any error message on the username.
+   */
+  uiUsernameSuccess (username) {
+    this.usernameError = undefined;
+  },
+  /**
+   * When the username fails validation, this sets the usernameError attribute
+   * which displays a message in the UI.
+   */
+  uiUsernameError (error) {
+    this.warn(error);
+    this.usernameError = {message: error.message};
+  },
 
   /**
    * `passwordField` allows you to change "password" to something else in
@@ -71,12 +94,35 @@ export default DefineMap.extend({
   passwordChanged (event) {
     this.password = event.target.value;
     this.onPasswordChange(event.target.value)
+      .then(password => this.uiPasswordSuccess(password))
+      .catch(error => this.uiPasswordError(error));
   },
   /**
    * The `onPasswordChange` callback gets called right after the password
    * is updated. It can be used to check if a password is valid.
    */
-  onPasswordChange (password) {},
+  onPasswordChange (password) {
+    return Promise.resolve(password);
+  },
+  /**
+   * If there is a problem with the password, `passwordError` can be set with an
+   * object that contains a 'message' attribute.
+   */
+  passwordError: 'any',
+  /**
+   * Clears any error message on the password.
+   */
+  uiPasswordSuccess (password) {
+    this.passwordError = undefined;
+  },
+  /**
+   * When the password fails validation, this sets the passwordError attribute
+   * which displays a message in the UI.
+   */
+  uiPasswordError (error) {
+    this.warn(error);
+    this.passwordError = {message: error.message};
+  },
 
   /**
    * If a strategy attribute is provided, it will be added to the request data.
