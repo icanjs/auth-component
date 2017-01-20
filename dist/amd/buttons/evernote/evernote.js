e2eb56357d080ce1dfd5d300504ee113376d6aca
Marshall Thompson <marshall@creativeideal.net>
2017-01-20 01:06:08 -0700

    5.0.1

---
+++ dist/amd/buttons/evernote/evernote.js
@@ -0,0 +1,14 @@
+/*auth-component@5.0.0#buttons/evernote/evernote*/
+define([
+    'exports',
+    './evernote.jsx',
+    'css!./evernote.less.css'
+], function (exports, _evernote) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    var _evernote2 = _interopRequireDefault(_evernote);
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : { default: obj };
+    }
+    exports.default = _evernote2.default;
+});
---
+++ dist/cjs/buttons/github/github.jsx.js
@@ -0,0 +1,23 @@
+/*auth-component@5.0.0#buttons/github/github.jsx!steal-react-jsx@0.0.3#steal-react-jsx*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var _react = require('react');
+var _react2 = _interopRequireDefault(_react);
+var _button = require('../button.jsx.js');
+var _button2 = _interopRequireDefault(_button);
+var _github = require('./github.svg.js');
+var _github2 = _interopRequireDefault(_github);
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+exports.default = function (_ref) {
+    var name = _ref.name, url = _ref.url, img = _ref.img, alt = _ref.alt, text = _ref.text, popup = _ref.popup;
+    return _react2.default.createElement(_button2.default, {
+        name: name || 'GitHub',
+        url: url || '/auth/github',
+        svg: _github2.default,
+        alt: alt,
+        text: text,
+        popup: popup
+    });
+};
